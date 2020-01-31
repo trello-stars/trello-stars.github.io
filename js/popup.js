01b11b788ab296065f59ac67ed3f77d88ea80708
blackyblack <blacky@mail.ru>
2020-02-01 01:56:06 +0200

    Show stars

--- js/popup.js
+++ js/popup.js
@@ -2,14 +2,18 @@
 
 var Promise = TrelloPowerUp.Promise;
 var t = TrelloPowerUp.iframe();
+var starsInput = document.getElementById('starsCount');
 
 t.render(function(){
-  return t.get('card', 'shared', 'stars')
-  .then(function([card, stars]) {
-    console.log("render card: " + card);
+  return t.get('card', 'shared', 'stars', 'null')
+  .then(function(stars) {
+    if(stars == 'null') {
+      stars = 0;
+    }
+    stars = parseInt(stars);
     console.log("stars: " + stars);
 
-    ///TODO: show stars on page
+    starsInput.value = "" + stars;
   })
   .then(function(){
     t.sizeTo('#content')
